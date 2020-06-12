27fb544bde7b094f3282b51b8ca24182bf771769
Mufeez Amjad <mufeez.amjad@outlook.com>
2020-06-11 20:59:41 -0400

    player implementation

--- src/Player.h
+++ src/Player.h
@@ -1,20 +1,24 @@
 #ifndef _PLAYER_
 #define _PLAYER_
 
+#include <unordered_set>
 #include <vector>
 
 #include "Card.h"
 #include "Command.h"
 
-enum PlayerNumber { ONE, TWO, THREE, FOUR };
+enum class PlayerNumber { ONE, TWO, THREE, FOUR };
 
 class Player {
 	public:
 		std::vector<Card*> hand;
 		std::vector<Card*> discards;
-		virtual Command playTurn(std::vector<Card*>&);
+		virtual Command playTurn(std::unordered_set<Card*>&) = 0;
 		void removeCard(Card*);
 		void setHand(Card*);
+	protected:
+		void discardCard(Card*);
+		std::vector<Card*> getValidTurns(std::unordered_set<Card*>&);
 };
 
 #endif
