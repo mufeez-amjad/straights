c0d5f5c8cb1d8a39b8eb6e4f73cf74d4cdf84f88
Paul Cento <prscento@uwaterloo.ca>
2020-06-16 17:35:09 -0400

    randomize seed if fixed seed is not read

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char* argv[])
 	// global variable seed in Shuffle.h
 	if (argc > 1)
 		seed = std::stoi(std::string(argv[1]));
+	else
+		seed = rand();
 
 	Game* game = Game::instance();
 	game->play();
