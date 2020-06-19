a5f67252299e23534232eba1fda1115f2b96d7a5
Paul Cento <prscento@uwaterloo.ca>
2020-06-18 22:06:37 -0400

    Refactor game

---
+++ src/Deck.h
@@ -0,0 +1,31 @@
+#ifndef _DECK_
+#define _DECK_
+
+#include "Card.h"
+
+extern int seed;
+
+class Deck
+{
+	friend std::ostream &operator<<(std::ostream&, const Deck&);
+
+	public:
+		Deck();
+		~Deck() noexcept;
+
+		void shuffle(void);
+		Card* at(unsigned int);
+
+		Card* lookup(Card);
+		Card* lookup(Card*);
+
+	private:
+		void _orderCards(void);
+
+		Card* _cards[CARD_COUNT];
+		Card* _orderedCards[CARD_COUNT];
+};
+
+std::ostream &operator<<(std::ostream&, const Deck&);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 
 #include "Game.h"
-#include "Shuffle.h"
-
-#include "Computer.h"
 
 int main(int argc, char* argv[])
 {
-	// global variable seed in Shuffle.h
+	// global variable seed in Deck.h
 	if (argc > 1)
 		seed = std::stoi(std::string(argv[1]));
 	else
